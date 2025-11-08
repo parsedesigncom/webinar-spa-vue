93f5f24f5c5a625ee4d3170457964c00877116c4
saeid.samani <saeid.samani@nsgroup.ch>
2025-11-08 20:23:15 +0100

    feat: integrate webinar store into HomeView with config and state display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,61 @@
 <script setup lang="ts">
-import { useCounterStore } from "../stores/counter"
+import { onMounted } from 'vue';
+import { useWebinarStore } from '@/stores/webinarStore';
 
-const counter = useCounterStore()
+const webinarStore = useWebinarStore();
 
-function add() {
-  counter.increment()
-}
+onMounted(() => {
+  webinarStore.loadWebinarConfig();
+});
 </script>
 
 <template>
-  <div>
-    <h1>Counter: {{ counter.count }}</h1>
-    <p>Double: {{ counter.double }}</p>
-    <button @click="add">+1</button>
-  </div>
+  <main>
+    <div v-if="webinarStore.isLoading">
+      Lade Webinar-Konfiguration...
+    </div>
+
+    <div v-else-if="webinarStore.error">
+      Fehler: {{ webinarStore.error }}
+    </div>
+
+    <section v-else>
+      <h1>Aktuelles Webinar</h1>
+      <div v-if="webinarStore.config">
+        <h1>config</h1>
+        <pre>{{ webinarStore.config }}</pre>
+      </div>
+      <div v-else>
+        No config found.
+      </div>
+      <div v-if="webinarStore.webinars">
+        <h1>webinars</h1>
+        <pre>{{ webinarStore.webinars }}</pre>
+      </div>
+      <div v-else>
+        No webinars found.
+      </div>
+      <div v-if="webinarStore.startWebinarId">
+        <h1>startWebinarId</h1>
+        <pre>{{ webinarStore.startWebinarId }}</pre>
+      </div>
+      <div v-else>
+        No startWebinarId found.
+      </div>
+      <div v-if="webinarStore.upcomingWebinars">
+        <h1>upcomingWebinars</h1>
+        <pre>{{ webinarStore.upcomingWebinars }}</pre>
+      </div>
+      <div v-else>
+        No upcomingWebinars found.
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-
+pre{
+  text-align: left;
+  font-size: 1.2em;
+}
 </style>
