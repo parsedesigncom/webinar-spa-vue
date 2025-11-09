f06abfe0016b24e5d8df896da3b1ff9875a915cd
saeid.samani <saeid.samani@nsgroup.ch>
2025-11-09 23:52:44 +0100

    chore: remove unused counter store and related views, update TypeScript config for consistent type declarations

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import router from "./router/index.ts";
+import router from "./router";
 import './style.css'
 import App from './App.vue'
 
